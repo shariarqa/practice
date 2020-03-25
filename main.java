157cb7ea755b21e4f4058f8f8988bc0d95ce6031
shariarqa <shariarqa@gmail.com>
2020-03-25 00:17:52 -0400

    Update main.java

--- main.java
+++ main.java
@@ -1,7 +1,7 @@
 public class main {
     public static void main(String[] args) {
-        int a = 2;
-        int b =2;
+        int a = 4;
+        int b =4;
         int sum = a+b;
         System.out.println(sum);
     }
